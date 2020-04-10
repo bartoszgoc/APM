db128aa8b9370cd7216ea3cc3dd766879c7704a4
bartosz.goc <bartosz.goc@signia.group>
2020-04-10 20:47:37 +0200

    implement product list component, toogleImage button

--- src/app/products/product-list.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from "@angular/core";
-
-@Component({
-    selector: "pm-products",
-    template: "./product-list.component.html"
-})
-export class ProductListComponent {
-    pageTitle: string = "Product List";
-}
