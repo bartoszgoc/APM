471885e23669471e5d71879d725bfae690948e50
bartosz.goc <bartosz.goc@signia.group>
2020-04-11 00:08:40 +0200

    implement filter products list method

--- src/app/product/product-list.component.ts
+++ src/app/product/product-list.component.ts
@@ -1,17 +1,29 @@
-import {Component} from "@angular/core";
-import {IProduct} from "./product";
+import { Component, OnInit } from "@angular/core";
+import { IProduct } from "./product";
 
 @Component({
     selector: "pm-products",
     templateUrl: "./product-list.component.html",
     styleUrls: ["./product-list.component.css"]
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+
+
     pageTitle: string = "Product List";
     imageWidth: number = 50;
-    imageMargin: number =2;
+    imageMargin: number = 2;
     showImage: boolean = false;
-    listFilter: string = "cart";
+
+    _listFilter: string;
+    get listFilter(): string {
+        return this._listFilter;
+    }
+    set listFilter(value: string) {
+        this._listFilter = value;
+        this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+    }
+
+    filteredProducts: IProduct[];
     products: IProduct[] = [
         {
             "productId": 1,
@@ -22,8 +34,8 @@ export class ProductListComponent {
             "price": 19.95,
             "starRating": 3.2,
             "imageUrl": "assets/images/leaf_rake.png"
-          },
-          {
+        },
+        {
             "productId": 2,
             "productName": "Garden Cart",
             "productCode": "GDN-0023",
@@ -32,10 +44,26 @@ export class ProductListComponent {
             "price": 32.99,
             "starRating": 4.2,
             "imageUrl": "assets/images/garden_cart.png"
-          }
+        }
     ];
+
+    constructor() {
+        this.filteredProducts = this.products;
+        this.listFilter = '';
+    }
+    performFilter(filterBy: string): IProduct[] {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product: IProduct) =>
+            product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1
+        );
+    }
 
     toggleImage(): void {
         this.showImage = !this.showImage;
     }
+
+    ngOnInit(): void {
+        console.log();
+    }
+    
 }
