63cdc1164a40d647e2187c7dd4f5ba68a9d9127b
bartosz.goc <bartosz.goc@signia.group>
2020-04-11 13:41:01 +0200

    replace hardcoded products list with product service output

--- dist/APM/main.js
+++ dist/APM/main.js
@@ -23,6 +23,28 @@ webpackEmptyAsyncContext.id = "./$$_lazy_route_resource lazy recursive";
 
 /***/ }),
 
+/***/ "./node_modules/raw-loader/index.js!./src/app/product/product-list.component.html":
+/*!*******************************************************************************!*\
+  !*** ./node_modules/raw-loader!./src/app/product/product-list.component.html ***!
+  \*******************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "<div class=\"card\">\r\n    <div class=\"card-header\">\r\n        {{pageTitle}}\r\n    </div>\r\n    <div class=\"card-body\">\r\n        <div class=\"row\">\r\n            <div class=\"col-md-2\">Filter by:</div>\r\n            <div class=\"col-md-4\">\r\n                <input type=\"text\" [(ngModel)] = 'listFilter'/>\r\n            </div>\r\n        </div>\r\n        <div class=\"row\">\r\n            <div class=\"col-md-6\">\r\n                <h4>Filtered by:{{listFilter}}</h4>\r\n            </div>\r\n        </div>\r\n        <div class=\"table-responsive\">\r\n            <table class=\"table\" *ngIf=\"products && products.length\">\r\n                <thead>\r\n                    <tr>\r\n                        <th>\r\n                            <button class=\"btn btn-primary\" (click)='toggleImage()'>\r\n                                {{showImage ? 'Hide' : 'Show'}} image\r\n                            </button>\r\n                        </th>\r\n                        <th>Product</th>\r\n                        <th>Code</th>\r\n                        <th>Available</th>\r\n                        <th>Price</th>\r\n                        <th>5 Star Ratinh</th>\r\n                    </tr>\r\n                </thead>\r\n                <tbody>\r\n                    <tr *ngFor=\"let product of filteredProducts\">\r\n                        <td>\r\n                            <img *ngIf='showImage' [src]='product.imageUrl' [title]='product.title'\r\n                                [style.width.px]='imageWidth' [style.margin.px]='imageMargin'>\r\n                        </td>\r\n                        <td>{{product.productName }} </td>\r\n                        <td>{{product.productCode | lowercase | convertToSpaces:'-'}}</td>\r\n                        <td>{{product.releaseDate}}</td>\r\n                        <td>{{product.price| currency:\"PLN\":symbol:\"2.2-2\"}}</td>\r\n                        <td><pm-star [rating]=\"product.starRating\" [productName]=\"product.productName\" (ratingClicked)=\"onRatingClicked($event)\"></pm-star></td>\r\n                    </tr>\r\n                </tbody>\r\n            </table>\r\n        </div>\r\n    </div>\r\n</div>"
+
+/***/ }),
+
+/***/ "./node_modules/raw-loader/index.js!./src/app/shared/star.component.html":
+/*!**********************************************************************!*\
+  !*** ./node_modules/raw-loader!./src/app/shared/star.component.html ***!
+  \**********************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "<div class=\"crop\"\n     [style.width.px]=\"starWidth\"\n     [title]=\"rating\"\n     (click)=\"onClick()\">\n  <div style=\"width: 75px\">\n    <span class=\"fa fa-star\"></span>\n    <span class=\"fa fa-star\"></span>\n    <span class=\"fa fa-star\"></span>\n    <span class=\"fa fa-star\"></span>\n    <span class=\"fa fa-star\"></span>\n  </div>\n</div>"
+
+/***/ }),
+
 /***/ "./src/app/app.component.ts":
 /*!**********************************!*\
   !*** ./src/app/app.component.ts ***!
@@ -70,8 +92,14 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
 /* harmony import */ var _angular_platform_browser__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/platform-browser */ "./node_modules/@angular/platform-browser/fesm2015/platform-browser.js");
 /* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm2015/core.js");
-/* harmony import */ var _app_component__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./app.component */ "./src/app/app.component.ts");
-/* harmony import */ var _products_product_list_component__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./products/product-list.component */ "./src/app/products/product-list.component.ts");
+/* harmony import */ var _angular_forms__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @angular/forms */ "./node_modules/@angular/forms/fesm2015/forms.js");
+/* harmony import */ var _app_component__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./app.component */ "./src/app/app.component.ts");
+/* harmony import */ var _product_product_list_component__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./product/product-list.component */ "./src/app/product/product-list.component.ts");
+/* harmony import */ var _shared_convert_to_spaces_pipe__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./shared/convert-to-spaces.pipe */ "./src/app/shared/convert-to-spaces.pipe.ts");
+/* harmony import */ var _shared_star_component__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ./shared/star.component */ "./src/app/shared/star.component.ts");
+
+
+
 
 
 
@@ -82,13 +110,16 @@ let AppModule = class AppModule {
 AppModule = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
     Object(_angular_core__WEBPACK_IMPORTED_MODULE_2__["NgModule"])({
         declarations: [
-            _app_component__WEBPACK_IMPORTED_MODULE_3__["AppComponent"],
-            _products_product_list_component__WEBPACK_IMPORTED_MODULE_4__["ProductListComponent"]
+            _app_component__WEBPACK_IMPORTED_MODULE_4__["AppComponent"],
+            _product_product_list_component__WEBPACK_IMPORTED_MODULE_5__["ProductListComponent"],
+            _shared_convert_to_spaces_pipe__WEBPACK_IMPORTED_MODULE_6__["ConvertToSpacesPipe"],
+            _shared_star_component__WEBPACK_IMPORTED_MODULE_7__["StarComponent"]
         ],
         imports: [
-            _angular_platform_browser__WEBPACK_IMPORTED_MODULE_1__["BrowserModule"]
+            _angular_platform_browser__WEBPACK_IMPORTED_MODULE_1__["BrowserModule"],
+            _angular_forms__WEBPACK_IMPORTED_MODULE_3__["FormsModule"]
         ],
-        bootstrap: [_app_component__WEBPACK_IMPORTED_MODULE_3__["AppComponent"]]
+        bootstrap: [_app_component__WEBPACK_IMPORTED_MODULE_4__["AppComponent"]]
     })
 ], AppModule);
 
@@ -96,10 +127,71 @@ AppModule = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
 
 /***/ }),
 
-/***/ "./src/app/products/product-list.component.ts":
+/***/ "./src/app/product/ProductService.ts":
+/*!*******************************************!*\
+  !*** ./src/app/product/ProductService.ts ***!
+  \*******************************************/
+/*! exports provided: ProductService */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "ProductService", function() { return ProductService; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm2015/core.js");
+
+
+let ProductService = class ProductService {
+    getProducts() {
+        return [
+            {
+                "productId": 5,
+                "productName": "Hammer",
+                "productCode": "TBX-0048",
+                "releaseDate": "May 21, 2019",
+                "description": "Curved claw steel hammer",
+                "price": 8.9,
+                "starRating": 4.8,
+                "imageUrl": "assets/images/hammer.png"
+            },
+            {
+                "productId": 10,
+                "productName": "Video Game Controller",
+                "productCode": "GMG-0042",
+                "releaseDate": "October 15, 2018",
+                "description": "Standard two-button video game controller",
+                "price": 35.95,
+                "starRating": 4.6,
+                "imageUrl": "assets/images/xbox-controller.png"
+            }
+        ];
+    }
+};
+ProductService = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+    Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["Injectable"])({
+        providedIn: "root"
+    })
+], ProductService);
+
+
+
+/***/ }),
+
+/***/ "./src/app/product/product-list.component.css":
 /*!****************************************************!*\
-  !*** ./src/app/products/product-list.component.ts ***!
+  !*** ./src/app/product/product-list.component.css ***!
   \****************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "thead {\r\n    color:#337Ab7\r\n}\r\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInNyYy9hcHAvcHJvZHVjdC9wcm9kdWN0LWxpc3QuY29tcG9uZW50LmNzcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtJQUNJO0FBQ0oiLCJmaWxlIjoic3JjL2FwcC9wcm9kdWN0L3Byb2R1Y3QtbGlzdC5jb21wb25lbnQuY3NzIiwic291cmNlc0NvbnRlbnQiOlsidGhlYWQge1xyXG4gICAgY29sb3I6IzMzN0FiN1xyXG59Il19 */"
+
+/***/ }),
+
+/***/ "./src/app/product/product-list.component.ts":
+/*!***************************************************!*\
+  !*** ./src/app/product/product-list.component.ts ***!
+  \***************************************************/
 /*! exports provided: ProductListComponent */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
@@ -108,19 +200,141 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "ProductListComponent", function() { return ProductListComponent; });
 /* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
 /* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm2015/core.js");
+/* harmony import */ var _ProductService__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./ProductService */ "./src/app/product/ProductService.ts");
+
 
 
 let ProductListComponent = class ProductListComponent {
-    constructor() {
+    constructor(productService) {
+        this.productService = productService;
         this.pageTitle = "Product List";
+        this.imageWidth = 50;
+        this.imageMargin = 2;
+        this.showImage = false;
+        this.products = [];
+    }
+    get listFilter() {
+        return this._listFilter;
+    }
+    set listFilter(value) {
+        this._listFilter = value;
+        this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+    }
+    onRatingClicked(message) {
+        this.pageTitle = message;
+    }
+    performFilter(filterBy) {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product) => product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+    }
+    toggleImage() {
+        this.showImage = !this.showImage;
+    }
+    ngOnInit() {
+        this.products = this.productService.getProducts();
+        this.filteredProducts = this.products;
     }
 };
+ProductListComponent.ctorParameters = () => [
+    { type: _ProductService__WEBPACK_IMPORTED_MODULE_2__["ProductService"] }
+];
 ProductListComponent = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
     Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["Component"])({
         selector: "pm-products",
-        template: "./product-list.component.html"
-    })
+        template: __webpack_require__(/*! raw-loader!./product-list.component.html */ "./node_modules/raw-loader/index.js!./src/app/product/product-list.component.html"),
+        styles: [__webpack_require__(/*! ./product-list.component.css */ "./src/app/product/product-list.component.css")]
+    }),
+    tslib__WEBPACK_IMPORTED_MODULE_0__["__metadata"]("design:paramtypes", [_ProductService__WEBPACK_IMPORTED_MODULE_2__["ProductService"]])
 ], ProductListComponent);
+
+
+
+/***/ }),
+
+/***/ "./src/app/shared/convert-to-spaces.pipe.ts":
+/*!**************************************************!*\
+  !*** ./src/app/shared/convert-to-spaces.pipe.ts ***!
+  \**************************************************/
+/*! exports provided: ConvertToSpacesPipe */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "ConvertToSpacesPipe", function() { return ConvertToSpacesPipe; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm2015/core.js");
+
+
+let ConvertToSpacesPipe = class ConvertToSpacesPipe {
+    transform(value, character) {
+        return value.replace(character, ' ');
+    }
+};
+ConvertToSpacesPipe = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+    Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["Pipe"])({
+        name: "convertToSpaces"
+    })
+], ConvertToSpacesPipe);
+
+
+
+/***/ }),
+
+/***/ "./src/app/shared/star.component.css":
+/*!*******************************************!*\
+  !*** ./src/app/shared/star.component.css ***!
+  \*******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = ".crop {\n  overflow: hidden;\n}\ndiv {\n  cursor: pointer;\n}\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInNyYy9hcHAvc2hhcmVkL3N0YXIuY29tcG9uZW50LmNzcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtFQUNFLGdCQUFnQjtBQUNsQjtBQUNBO0VBQ0UsZUFBZTtBQUNqQiIsImZpbGUiOiJzcmMvYXBwL3NoYXJlZC9zdGFyLmNvbXBvbmVudC5jc3MiLCJzb3VyY2VzQ29udGVudCI6WyIuY3JvcCB7XG4gIG92ZXJmbG93OiBoaWRkZW47XG59XG5kaXYge1xuICBjdXJzb3I6IHBvaW50ZXI7XG59Il19 */"
+
+/***/ }),
+
+/***/ "./src/app/shared/star.component.ts":
+/*!******************************************!*\
+  !*** ./src/app/shared/star.component.ts ***!
+  \******************************************/
+/*! exports provided: StarComponent */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "StarComponent", function() { return StarComponent; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm2015/core.js");
+
+
+let StarComponent = class StarComponent {
+    constructor() {
+        this.ratingClicked = new _angular_core__WEBPACK_IMPORTED_MODULE_1__["EventEmitter"]();
+    }
+    ngOnChanges() {
+        this.starWidth = this.rating * 75 / 5;
+    }
+    onClick() {
+        this.ratingClicked.emit(`The rating of ${this.productName} is qual to ${this.rating}`);
+    }
+};
+tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+    Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["Input"])(),
+    tslib__WEBPACK_IMPORTED_MODULE_0__["__metadata"]("design:type", Number)
+], StarComponent.prototype, "rating", void 0);
+tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+    Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["Input"])(),
+    tslib__WEBPACK_IMPORTED_MODULE_0__["__metadata"]("design:type", String)
+], StarComponent.prototype, "productName", void 0);
+tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+    Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["Output"])(),
+    tslib__WEBPACK_IMPORTED_MODULE_0__["__metadata"]("design:type", _angular_core__WEBPACK_IMPORTED_MODULE_1__["EventEmitter"])
+], StarComponent.prototype, "ratingClicked", void 0);
+StarComponent = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+    Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["Component"])({
+        selector: "pm-star",
+        template: __webpack_require__(/*! raw-loader!./star.component.html */ "./node_modules/raw-loader/index.js!./src/app/shared/star.component.html"),
+        styles: [__webpack_require__(/*! ./star.component.css */ "./src/app/shared/star.component.css")]
+    })
+], StarComponent);
 
 
 
